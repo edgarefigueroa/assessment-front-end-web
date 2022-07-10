6078a44eaa0b384f038bc74076b3001e49b5624b
Edgar Figueroa <edgarefigueroa@outlook.com>
2022-07-10 18:29:32 -0400

    style buttons

--- profile.js
+++ profile.js
@@ -1,9 +1,28 @@
 function handleSubmit(evt) {
 	evt.preventDefault();
 
-	alert('Form has been submitted successfully!');
+	alert('That is an awsome place!');
 }
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+//display alert function
+function favColor(evt) {
+	evt.preventDefault();
+
+	alert('Green!');
+}
+
+//display alert arrow function
+const favPlace = () => alert(`Puerto Vallarta!`)
+const favRitual = () => alert(`Walking`)
+
+let favColorBtn  = document.querySelector('#color')
+let favPlaceBtn  = document.querySelector('#place')
+let favRitualBtn  = document.querySelector('#ritual')
+
+favColorBtn.addEventListener('click', favColor)
+favPlaceBtn.addEventListener('click', favPlace)
+favRitualBtn.addEventListener('click', favRitual)
